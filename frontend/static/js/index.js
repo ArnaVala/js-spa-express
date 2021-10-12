cf31159ab5386481eb9104b2b4a1dbae0cd6b002
ArnaVala <arnavala@gmail.com>
2021-10-12 22:15:59 +0000

    add function to find matching route to path

--- frontend/static/js/index.js
+++ frontend/static/js/index.js
@@ -1,2 +1,31 @@
 //client-side entry point
-console.log('works')
+//console.log('works')
+
+// client-side router
+// load views in router - with async function
+const router = async () => {
+  //define routes array
+  const routes = [
+    { path: "/", view: () => console.log('viewing dashboard') },
+    { path: "/posts", view: () => console.log('viewing posts') },
+    { path: "/settings", view: () => console.log('viewing settings') },
+  ];
+
+  // Test each route - for match
+  const potentialMatches = routes.map(route => {
+    return {
+      route: route,
+      isMatch: location.pathname === route.path
+    }
+  });
+
+  // find matching route after looping through all routes (boolean true or false)
+  let match = potentialMatches.find(potentialMatch => potentialMatch.isMatch);
+
+  console.log(match);
+};
+
+// call the router func on dom load
+document.addEventListener('DOMContentLoaded', () => {
+  router();
+});
