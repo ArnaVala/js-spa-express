4ea361728e816794822172a3e25fcf144a9ae026
ArnaVala <arnavala@gmail.com>
2021-10-12 21:26:18 +0000

    add static/js for client-side

--- server.js
+++ server.js
@@ -4,10 +4,13 @@ const path = require("path");
 
 const app = express();
 
+// use middleware to serve the static dir - whenever a file has static/
+app.use("/static", express.static(path.resolve(__dirname, "frontend", "static")))
+
 // Send all paths ('/*') - all paths are sent back to the index.html -
 // the paths are still /settings - /posts etc.
 app.get("/*", (req, res) => {
-  res.sendFile(path.resolve("frontend", "index.html"));
+  res.sendFile(path.resolve(__dirname, "frontend", "index.html"));
 });
 
 // Start server - run 'node server.js' in terminal
